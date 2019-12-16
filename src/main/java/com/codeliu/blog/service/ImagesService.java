0de4f60d14e5710e33af7ebe15aff79361ac59fb
CodeTiger <18252025311@163.com>
2019-12-16 22:29:45 +0800

    Add list image interface

--- src/main/java/com/codeliu/blog/service/ImagesService.java
+++ src/main/java/com/codeliu/blog/service/ImagesService.java
@@ -3,6 +3,7 @@ package com.codeliu.blog.service;
 import com.codeliu.blog.util.ResultUtils;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
 import java.util.Map;
 
 public interface ImagesService {
@@ -10,4 +11,6 @@ public interface ImagesService {
     ResultUtils<Map<String, Object>> addImage(MultipartFile file);
 
     ResultUtils<Map<String, Object>> deleteImage(Integer imageId);
+
+    ResultUtils<List<Map<String, Object>>> listImage();
 }
